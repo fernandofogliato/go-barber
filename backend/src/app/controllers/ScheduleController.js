72bda83efcf5af861146b3d51db92e617cb6a9cf
Fernando Fogliato <fernandof@attornatus.com.br>
2019-11-20 22:53:12 -0300

    Add react frontend

--- backend/src/app/controllers/ScheduleController.js
+++ backend/src/app/controllers/ScheduleController.js
@@ -3,7 +3,6 @@ import { Op } from 'sequelize';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
-import File from '../models/File';
 
 class ScheduleController {
   async index(req, res) {
@@ -33,9 +32,6 @@ class ScheduleController {
           model: User,
           as: 'user',
           attributes: ['id', 'name'],
-          include: [
-            { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
-          ],
         },
       ],
     });
