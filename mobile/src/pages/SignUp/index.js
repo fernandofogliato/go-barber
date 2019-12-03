6ed179b54cb664aa86d35c239a23f253b726a7d2
Fernando Fogliato <fernandof@attornatus.com.br>
2019-12-02 23:06:11 -0300

    Add mobile application

---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,75 @@
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Informe seu nome"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Informe seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Informe sua senha"
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Confirme sua senha"
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Já tenho conta</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
+}
