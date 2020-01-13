4dd126ae8acdbe13fb9a97bba6a3ca5eeeb68c74
Fernando Fogliato <fernandofogliato@gmail.com>
2020-01-13 11:29:46 -0300

    Fix security issues

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 
 import cors from 'cors';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
@@ -25,12 +29,28 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 900000,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
